620d66adbacee444d16982a841930d461146c4be
Teun <teun2408@gmail.com>
2018-06-11 13:50:19 +0200

    heroku aanpasingen

--- config/env/env.js
+++ config/env/env.js
@@ -20,6 +20,22 @@ let configStream = {
     http:{
         port: 8000,
         allow_origin: '*'
+    },
+    trans: {
+        ffmpeg: './config/ffmpeg2',
+        tasks: [
+            {
+                port: 5000,
+                app: 'live',
+                ac: 'aac',
+                hls: true,
+                hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+                dash: true,
+                dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+                mp4: true,
+                mp4Flags: '[movflags=faststart]'
+            }
+        ]
     }
 
 };
