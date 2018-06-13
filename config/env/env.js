27035bc9a339b47702d4c92e39dd72a423ea29ef
baspalinckx <bas.palinckx@hotmail.nl>
2018-06-13 13:25:40 +0200

    changed cmd

--- config/env/env.js
+++ config/env/env.js
@@ -14,17 +14,7 @@ const ffmpegSrc = process.platform=== 'win32'?
 
 console.log(ffmpegSrc);
 
-const { exec } = require('child_process');
-exec('ffmpeg -v verbose -i rtmp://188.166.29.146/live/test -c:v libx264 -c:a aac -ac 1 -strict -2 -crf 18 -profile:v baseline -maxrate 400k -bufsize 1835k -pix_fmt yuv420p -flags -global_header -hls_time 10 -hls_list_size 6 -hls_wrap 10 -start_number 1 /home/stream/public_html/test/index.m3u8', (err, stdout, stderr) => {
-    if (err) {
-        // node couldn't execute the command
-        return;
-    }
 
-    // the *entire* stdout and stderr (buffered)
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-});
 
 
 let configStream = {
